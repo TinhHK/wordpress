fb34b59e6d7c5abd01ed6bf00fc5abac8e93bb8d
TinhHK <mrktinh@gmail.com>
2021-01-09 16:19:19 +0700

    remove actions in hook, check the existing of an action

--- wp-content/plugins/mrk-plugin/lessons/2_action_hook.php
+++ wp-content/plugins/mrk-plugin/lessons/2_action_hook.php
@@ -13,7 +13,7 @@ function mrk_new_one()
 }
 
 //======== add css to header if the page is the type of a page=========//
-add_action('wp_head', 'mrk_add_css');
+add_action('wp_head', 'mrk_add_css', 19);
 function mrk_add_css()
 {
     if(is_page()){
@@ -22,3 +22,12 @@ function mrk_add_css()
         echo $css;
     }
 }
+
+//============= remove action after add action ===================
+remove_action('wp_head', 'mrk_add_css', 19);
+
+//============= remove all action in a hook ======================
+remove_all_actions('wp_head');
+
+//============ check action is existed or not, return priority =================
+has_action('wp_head', 'mrk_add_css');
