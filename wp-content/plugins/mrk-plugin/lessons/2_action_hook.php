0b047e76e806e1227d4bd6c6a135bdb865d98d82
TinhHK <mrktinh@gmail.com>
2021-01-09 15:41:14 +0700

    action hook add css to head of the page

--- wp-content/plugins/mrk-plugin/lessons/2_action_hook.php
+++ wp-content/plugins/mrk-plugin/lessons/2_action_hook.php
@@ -1,1 +1,24 @@
 <?php
+// ========== action hook with priority same not overridden===============
+add_action('wp_footer', 'mrk_new_data', 19);
+add_action('wp_footer', 'mrk_new_one', 20);
+add_action('wp_footer', 'mrk_new_data', 21);
+function mrk_new_data()
+{
+    echo "<div>Hello Mrktinh</div>";
+}
+function mrk_new_one()
+{
+    echo "<div>Hello you again</div>";
+}
+
+//======== add css to header if the page is the type of a page=========//
+add_action('wp_head', 'mrk_add_css');
+function mrk_add_css()
+{
+    if(is_page()){
+        $cssUrl = plugins_url('/css/my-style.css', __FILE__);
+        $css = '<link rel="alternate" href="'.$cssUrl.'">';
+        echo $css;
+    }
+}
--- wp-content/plugins/mrk-plugin/mrk-plugin.php
+++ wp-content/plugins/mrk-plugin/mrk-plugin.php
@@ -8,16 +8,6 @@ Version: 1.0
 Author URI: http://mrktinh.com
  */
 
-// ========== action hook with priority ===============
-add_action('wp_footer', 'mrk_new_data', 19);
-add_action('wp_footer', 'mrk_new_one', 20);
-add_action('wp_footer', 'mrk_new_data', 21);
-function mrk_new_data()
-{
-    echo "<div>Hello Mrktinh</div>";
-}
-function mrk_new_one()
-{
-    echo "<div>Hello you again</div>";
-}
+
+
 
