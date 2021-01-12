413ca3bf8b02e123e9b8ec1ac2c603127a3d7921
TinhHK <mrktinh@gmail.com>
2021-01-12 23:32:54 +0700

    change the title of the post having id = 1

--- wp-content/plugins/mrk-plugin/includes/public.php
+++ wp-content/plugins/mrk-plugin/includes/public.php
@@ -1,13 +1,1 @@
 <?php
-class MrkMP
-{
-    public function __construct()
-    {
-        add_filter('the_title', [$this, 'changeTitle']);
-
-    }
-    public function changeTitle()
-    {
-        return "Title was changed";
-    }
-}
--- wp-content/plugins/mrk-plugin/lessons/3_filter_hook.php
+++ wp-content/plugins/mrk-plugin/lessons/3_filter_hook.php
@@ -1,7 +1,18 @@
 <?php
-//======== add filter hook to change title =============
-add_filter('the_title', 'changeTitle');
-function changeTitle()
+//======== add filter hook to change title with post having id = 1 =============
+class MrkMP
 {
-    return 'This is changed title';
+    public function __construct()
+    {
+        // 2 is the number of argument in the_title hook
+        add_filter('the_title', [$this, 'changeTitle'], 10, 2);
+
+    }
+    public function changeTitle($title, $id)
+    {
+        if($id == 1){
+            return str_replace('Hello', 'Xin chao', $title);
+        }
+        return $title;
+    }
 }
