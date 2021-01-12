1b549e1d02f64c5029748df05898b3d5c5b4f969
TinhHK <mrktinh@gmail.com>
2021-01-13 00:21:59 +0700

    remove and remove all filter of any hook

--- wp-content/plugins/mrk-plugin/lessons/3_filter_hook.php
+++ wp-content/plugins/mrk-plugin/lessons/3_filter_hook.php
@@ -8,6 +8,10 @@ class MrkMP
         add_filter('the_title', [$this, 'changeTitle'], 10, 2);
         add_action('wp_footer', [$this, 'showFunction']);
         add_filter('the_content', [$this, 'changeContent']);
+        // remove filter hook convert_smilles
+        remove_filter('the_content', 'convert_smilies');
+        // remove all filter of the content hook with priority is 10, other priorities are not removed
+        remove_all_filters('the_content', 10);
 
     }
     //======== add filter hook to change title with post having id = 1 =============
