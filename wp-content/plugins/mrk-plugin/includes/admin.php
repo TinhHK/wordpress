74d075f89de2940faedbca7501e9e9d63ca2daa9
TinhHK <mrktinh@gmail.com>
2021-01-17 22:32:56 +0700

    add section, add fields

--- wp-content/plugins/mrk-plugin/includes/admin.php
+++ wp-content/plugins/mrk-plugin/includes/admin.php
@@ -2,30 +2,50 @@
 require_once MRK_PLUGIN_DIR.'/includes/support.php';
 class MrkMpAdmin
 {
+    private $_menuSlug = 'mrktinh-plugin-setting';
     public function __construct()
     {
         add_action('admin_menu', [$this, 'addMenu']);
+        add_action('admin_init', [$this, 'registerSettingAndFields']);
 
     }
     // register setting and fields
     public function registerSettingAndFields()
     {
-        register_setting('mrktinh_plugin_options', 'mrktinh_plugin_name', [$this, 'validateSetting']);
+        register_setting('mrktinh_plugin_options', 'mrktinh_plugin_name',
+            [$this, 'validateSetting']);
+        $mainSection = 'mrk_plg_main_section';
+        $extendSection = 'mrk_plg_extend_section';
+        // add section to view
+        add_settings_section($mainSection, 'Main setting',
+            [$this, 'main_section_view'], $this->_menuSlug);
+        // add a field to section
+        add_settings_field('mrk_plg_new_title', 'Site title',
+            [$this, 'new_title_input'], $this->_menuSlug, $mainSection);
+        add_settings_section($extendSection, 'Extend setting',
+            [$this, 'main_section_view'], $this->_menuSlug);
     }
     public function validateSetting()
     {
 
     }
+    public function main_section_view()
+    {
+
+    }
+    public function new_title_input()
+    {
+        echo '<input type="text" name="mrktinh_plugin_name[mrk_plg_new_title]" value=""/>';
+    }
 
     // add menu
     public function addMenu()
     {
-        $slug = 'mrktinh-plugin-setting';
         add_menu_page(
             'Mrktinh setting title',
             'Mrktinh Plugin Setting',
             'manage_options',
-            $slug,
+            $this->_menuSlug,
             [$this, 'settingPage'],
             '',
             1
--- wp-content/plugins/mrk-plugin/views/setting-page.php
+++ wp-content/plugins/mrk-plugin/views/setting-page.php
@@ -3,5 +3,6 @@
     <p>Display configuration of Mrktinh Plugin </p>
     <form action="options.php" method="post" id="mrktinh-form-setting" enctype="multipart/form-data">
         <?php  echo settings_fields('mrktinh_plugin_options') ?>
+        <?php echo do_settings_sections($this->_menuSlug) ?>
     </form>
 </div>
