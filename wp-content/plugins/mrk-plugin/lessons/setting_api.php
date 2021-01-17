ae1cb66d34af6db6ebe916500c295f69e64a4ec3
TinhHK <mrktinh@gmail.com>
2021-01-18 01:12:44 +0700

    upload file

--- wp-content/plugins/mrk-plugin/lessons/setting_api.php
+++ wp-content/plugins/mrk-plugin/lessons/setting_api.php
@@ -17,6 +17,7 @@ class MrkMpAdmin
         register_setting('mrktinh_plugin_options', 'mrktinh_plugin_name',
             [$this, 'validateSetting']);
         $mainSection = 'mrk_plg_main_section';
+        $extendSection = 'mrk_plg_extend_section';
 
         // add section to view MAIN SETTING
         add_settings_section($mainSection, 'Main setting',
@@ -24,16 +25,19 @@ class MrkMpAdmin
         // add a field to section
         add_settings_field('mrk_plg_new_title', 'Site title',
             [$this, 'new_title_input'], $this->_menuSlug, $mainSection);
-        add_settings_field('mrk_plg_logo', 'Logo',
-                            [$this, 'logo_input'], $this->_menuSlug, $mainSection);
+
+        // EXTEND SETTING
+        add_settings_section($extendSection, 'Extend setting',
+            [$this, 'main_section_view'], $this->_menuSlug);
+        add_settings_field('mrk_plg_slogan', 'Slogan',
+            [$this, 'slogan_input'], $this->_menuSlug, $extendSection);
+        // add field with no section
+        add_settings_field('mrk_plg_security_code', '',
+            [$this, 'security_code_input'], $this->_menuSlug, 'custom-section');
     }
     public function validateSetting($inputs)
     {
-        if(isset($_FILES['mrk_plg_logo']['name'])){
-            $override = ['test_form' => false];
-            $fileInfo = wp_handle_upload($_FILES['mrk_plg_logo'], $override);
-            $inputs['mrk_plg_log'] = $fileInfo['url'];
-        }
+        update_option('mrk_plg_slogan', $_POST['mrk_plg_slogan']);
         return $inputs;
     }
     public function main_section_view()
@@ -45,21 +49,29 @@ class MrkMpAdmin
         echo '<input type="text" name="mrktinh_plugin_name[mrk_plg_new_title]" 
             value="'.$this->_settingOptions['mrk_plg_new_title'].'"/>';
     }
-    public function logo_input()
+    public function slogan_input()
     {
-        echo '<input type="file" name="mrk_plg_logo" />';
+        $slogan = get_option('mrk_plg_slogan', '');
+        echo '<input type="text" name="mrk_plg_slogan" 
+            value="'.$slogan.'"/>';
     }
-
+    public function security_code_input()
+    {
+        echo 'Security code <br>';
+        echo '<input type="text" name="mrktinh_plugin_name[mrk_plg_security_code]" value=""/>';
+    }
 
     // add menu
     public function addMenu()
     {
-        add_options_page(
+        add_menu_page(
             'Mrktinh setting title',
             'Mrktinh Plugin Setting',
             'manage_options',
             $this->_menuSlug,
             [$this, 'settingPage'],
+            '',
+            1
         );
     }
     // content of the menu
