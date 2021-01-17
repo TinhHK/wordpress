c80418c4d4cecfdff6cb936a6406f08fafc208b0
TinhHK <mrktinh@gmail.com>
2021-01-17 20:44:15 +0700

    create menu, register_setting, register_field

--- wp-content/plugins/mrk-plugin/includes/admin.php
+++ wp-content/plugins/mrk-plugin/includes/admin.php
@@ -2,5 +2,39 @@
 require_once MRK_PLUGIN_DIR.'/includes/support.php';
 class MrkMpAdmin
 {
+    public function __construct()
+    {
+        add_action('admin_menu', [$this, 'addMenu']);
+
+    }
+    // register setting and fields
+    public function registerSettingAndFields()
+    {
+        register_setting('mrktinh_plugin_options', 'mrktinh_plugin_name', [$this, 'validateSetting']);
+    }
+    public function validateSetting()
+    {
+
+    }
+
+    // add menu
+    public function addMenu()
+    {
+        $slug = 'mrktinh-plugin-setting';
+        add_menu_page(
+            'Mrktinh setting title',
+            'Mrktinh Plugin Setting',
+            'manage_options',
+            $slug,
+            [$this, 'settingPage'],
+            '',
+            1
+        );
+    }
+    // content of the menu
+    public function settingPage()
+    {
+        require_once MRK_PLUGIN_DIR.'/views/setting-page.php';
+    }
 
 }
--- wp-content/plugins/mrk-plugin/views/setting-page.php
+++ wp-content/plugins/mrk-plugin/views/setting-page.php
@@ -1,2 +1,7 @@
-
-<h2> Mrktinh setting </h2>
+<div class="wrap">
+    <h2>Mrktinh Plugin Setting</h2>
+    <p>Display configuration of Mrktinh Plugin </p>
+    <form action="options.php" method="post" id="mrktinh-form-setting" enctype="multipart/form-data">
+        <?php  echo settings_fields('mrktinh_plugin_options') ?>
+    </form>
+</div>
